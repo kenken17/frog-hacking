4c9efe4370a90a587795523f67f708942f6b29d9
ken <kenken17@gmail.com>
2021-01-06 00:55:00 +0800

    Add in the initial tmux script for CTF

---
+++ new_ctf.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+if [ -z $1 ]; then
+  echo "CTF Name?"
+else
+  mkdir -p ~/ctf/$1
+  cd ~/ctf/$1
+
+  touch nmap
+  touch notes.md
+
+  SESSION=$1
+  SESSION_EXISTS=$(tmux list-sessions | grep $SESSION)
+
+  if [ "$SESSION_EXISTS" = "" ]; then
+    # Main window
+    tmux new-session -d -s "$SESSION"
+    tmux rename-window -t 1 'ovpn' # current tmux index start with 1
+
+    # Note taking window
+    tmux new-window -t "$SESSION":2 -n 'notes'
+    tmux send-keys -t 'notes' 'vim notes.md' Enter
+    tmux send-keys -t 'notes' ':q' Enter # to quit the nerdtree
+
+    # nmap window
+    tmux new-window -t "$SESSION":3 -n 'nmap'
+
+    if [ -z $2 ]; then
+      tmux send-keys -t 'nmap' "nmap -sV -oN nmap "
+    else
+      tmux send-keys -t 'nmap' "nmap -sV -oN nmap $2" Enter
+    fi
+
+    tmux split-window -t 'nmap' -h
+    tmux select-pane -t 'nmap' -R
+
+    if [ -z $2 ]; then
+      tmux send-keys -t 'nmap.right' "gobuster dir -e -w $W_COMMON -x php,html,js,css,cgi,txt,sh,py,rb -u http://"
+    else
+      tmux send-keys -t 'nmap.right' "gobuster dir -e -w $W_COMMON -x php,html,js,css,cgi,txt,sh,py,rb -u http://$2" Enter
+    fi
+
+    # Hacking window
+    tmux new-window -t "$SESSION":4 -n 'nc'
+    tmux send-keys -t 'nc' 'nc -lvp 4444'
+  fi
+
+  # Start hacking!
+  tmux attach-session -t "$SESSION":3
+fi
