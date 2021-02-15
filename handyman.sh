12a90ddb47f33fc7345d274da096788086cca822
ken <kenken17@gmail.com>
2021-02-15 22:31:55 +0800

    Add more commands

--- handyman.sh
+++ handyman.sh
@@ -1,20 +1,24 @@
 #! /bin/bash
 
+EXTS=.asp,.aspx,.bat,.cgi,.htm,.html,.js,.log,.php,.phtml,.sh,.sql,.txt,.xml
+
 COMMAND_1='nmap -sV -sC $TARGET | highlight "^\d+\/tcp"'
-COMMAND_2='ffuf -v -c -recursion -t 64 -w "$W_COMMON" -u http://$TARGET/FUZZ'
-COMMAND_3='hydra -f -I -vV -t 64 -L "$W_USERNAME" -P "$W_PASSWORD" $TARGET ssh -s PORT'
-COMMAND_4='hydra -f -I -vV -t 64 -L "$W_USERNAME" -P "$W_PASSWORD" $TARGET http-post-form "/login.php:username=^USER^&password=^PASS^&login=Submit:F=Login failed"'
-COMMAND_5='ssh $USER@$TARGET -p 22'
-COMMAND_6='scp ~/Tools/linpeas.sh $USER@$TARGET:/tmp'
-COMMAND_7='ffuf -v -t 8 -X POST -w $W_COMMON -u http://$TARGET/login.php -d "key=FUZZ"'
-COMMAND_8='fcrackzip -D -v -p "$W_PASSWORD" file.zip'
-COMMAND_9='binwalk -e file.jpg'
-COMMAND_a='steghide extract -sf file.jpg'
-COMMAND_b='7z x file.zip'
-COMMAND_c='john --wordlist=$W_PASSWORD --format=FORMAT hash.txt'
-COMMAND_d='python /usr/share/john/ssh2john.py key > key.hash'
-COMMAND_e='nc -nlvp 4444'
-COMMAND_f='scp ~/Tools/handyman.sh $USER@$TARGET:/tmp'
+COMMAND_2='ffuf -v -c -recursion -t 64 -e $EXTS -w "$W_COMMON" -u http://$TARGET/FUZZ'
+COMMAND_3='gobuster dir -e -l -t 64 -w "$W_COMMON" -x $EXTS -u http://$TARGET | highlight "200|30[12]"'
+COMMAND_4='whatweb =v $TARGET'
+COMMAND_5='hydra -f -I -vV -t 64 -L "$W_USERNAME" -P "$W_PASSWORD" $TARGET ssh -s PORT'
+COMMAND_6='hydra -f -I -vV -t 64 -L "$W_USERNAME" -P "$W_PASSWORD" $TARGET http-post-form "/login.php:username=^USER^&password=^PASS^&login=Submit:F=Login failed"'
+COMMAND_7='ssh $USER@$TARGET -p 22'
+COMMAND_8='scp ~/Tools/linpeas.sh $USER@$TARGET:/tmp'
+COMMAND_9='ffuf -v -t 8 -X POST -w $W_COMMON -u http://$TARGET/login.php -d "key=FUZZ"'
+COMMAND_a='fcrackzip -D -v -p "$W_PASSWORD" file.zip'
+COMMAND_b='binwalk -e file.jpg'
+COMMAND_c='steghide extract -sf file.jpg'
+COMMAND_d='7z x file.zip'
+COMMAND_e='python /usr/share/john/ssh2john.py key > key.hash'
+COMMAND_f='john --wordlist=$W_PASSWORD --format=FORMAT hash.txt'
+COMMAND_g='nc -nlvp 4444'
+COMMAND_h='scp ~/Tools/handyman.sh $USER@$TARGET:/tmp'
 
 show_menus() {
   clear
@@ -36,6 +40,8 @@ show_menus() {
   echo "  d) $COMMAND_d"
   echo "  e) $COMMAND_e"
   echo "  f) $COMMAND_f"
+  echo "  g) $COMMAND_g"
+  echo "  h) $COMMAND_h"
   echo ""
   echo "  x. Exit"
   echo ""
@@ -43,6 +49,7 @@ show_menus() {
 
 runV() {
   tmux setenv TARGET $TARGET
+  tmux setenv USER $USER
 
   tmux split-window -v
   sleep 0.1
@@ -52,6 +59,7 @@ runV() {
 
 runH() {
   tmux setenv TARGET $TARGET
+  tmux setenv USER $USER
 
   tmux split-window -h
   sleep 0.1
@@ -64,50 +72,22 @@ read_options() {
   read -p "Enter choice " choice
   case $choice in
     1) runV $COMMAND_1;;
-    1-) runV $COMMAND_1;;
-    1_) runH $COMMAND_1;;
     2) runV $COMMAND_2;;
-    2-) runV $COMMAND_2;;
-    2_) runH $COMMAND_2;;
     3) runV $COMMAND_3;;
-    3-) runV $COMMAND_3;;
-    3_) runH $COMMAND_3;;
     4) runV $COMMAND_4;;
-    4-) runV $COMMAND_4;;
-    4_) runH $COMMAND_4;;
     5) runV $COMMAND_5;;
-    5-) runV $COMMAND_5;;
-    5_) runH $COMMAND_5;;
     6) runV $COMMAND_6;;
-    6-) runV $COMMAND_6;;
-    6_) runH $COMMAND_6;;
     7) runV $COMMAND_7;;
-    7-) runV $COMMAND_7;;
-    7_) runH $COMMAND_7;;
     8) runV $COMMAND_8;;
-    8-) runV $COMMAND_8;;
-    8_) runH $COMMAND_8;;
     9) runV $COMMAND_9;;
-    9-) runV $COMMAND_9;;
-    9_) runH $COMMAND_9;;
     a) runV $COMMAND_a;;
-    a-) runV $COMMAND_a;;
-    a_) runH $COMMAND_a;;
     b) runV $COMMAND_b;;
-    b-) runV $COMMAND_b;;
-    b_) runH $COMMAND_b;;
     c) runV $COMMAND_c;;
-    c-) runV $COMMAND_c;;
-    c_) runH $COMMAND_c;;
     d) runV $COMMAND_d;;
-    d-) runV $COMMAND_d;;
-    d_) runH $COMMAND_d;;
     e) runV $COMMAND_e;;
-    e-) runV $COMMAND_e;;
-    e_) runH $COMMAND_e;;
     f) runV $COMMAND_f;;
-    f-) runV $COMMAND_f;;
-    f_) runH $COMMAND_f;;
+    g) runV $COMMAND_g;;
+    h) runV $COMMAND_h;;
     x) exit 0;;
     *) echo -e "Error..."
   esac
